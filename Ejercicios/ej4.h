79b49f36e041604a26de578fa9826f18aa771554
agustinpatruno <agustinpatruno@gmail.com>
2025-03-18 18:32:53 -0300

    actualizando el .h del ejercicio 4

--- Ejercicios/ej4.h
+++ Ejercicios/ej4.h
@@ -10,28 +10,37 @@ using namespace std;
 input: const char* texto1, const char* texto2
 ouput: bool
 explicacion: funcion recursiva que compara si 2 cadenas de texto son iguales o no
-(true si lo son, false en caso contrario). Ademas con constexpr se ejecuta en tiempos de compilacion
+(true si lo son, false en caso contrario). Ademas con constexpr se ejecuta en tiempos de compilacion.
 
 */
-constexpr bool comparar_textos(const char* texto1,const char* texto2);
+constexpr bool comparar_textos_ejecucion(const char* texto1,const char* texto2);
 
 
 /*
 input: const char* texto1, const char* texto2
 ouput: bool
 explicacion: funcion recursiva que compara si 2 cadenas de texto son iguales o no
-(true si lo son, false en caso contrario)
+(true si lo son, false en caso contrario). se ejecuta normalmente.
 
 */
-bool comparar_textos_ejecucion(const char* texto1,const char* texto2);
+bool comparar_textos(const char* texto1,const char* texto2);
 
 
 /*
-input: const char* texto1, const char* texto2, bool comparar
+input: const char* texto1, const char* texto2
 ouput: void
 explicacion: llama a la funcion comparar_textos(const char* texto1,const char* texto2) para calcular el 
-tiempo que tarda en ejecutarse y dependiendo el bool que le pasemos, luego llamara a la funcion 
-comparar_textos_ejecucion(const char* texto1,const char* texto2) para comparar el tiempo de diferencia entre ambas
+tiempo que tarda en ejecutarse normalmente
 
 */
-void miprocesoAmedir(const char* texto1, const char* texto2, bool comparar);
+void miprocesoAmedir_normal(const char* texto1, const char* texto2);
+
+
+/*
+input: const char* texto1, const char* texto2
+ouput: void
+explicacion: llama a la funcion comparar_textos_ejecucion(const char* texto1,const char* texto2) para calcular el 
+tiempo que tarda en ejecutarse en tiempo de compilacion
+
+*/
+void miprocesoAmedir_ejecucion();
